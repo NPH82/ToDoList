28c5467bc9a9ee717ff18a862bc75705596214d8
NPH82 <nich1982@gmail.com>
2018-08-31 19:30:18 -0400

    Setup for todo list

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = `List of Things "to do"`;
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
